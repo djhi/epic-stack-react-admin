aea7de1fae984c294be753761c92c956b616d384
Gildas Garcia <1122076+djhi@users.noreply.github.com>
2023-10-08 11:25:50 +0200

    Initial commit

---
+++ remix.config.js
@@ -0,0 +1,25 @@
+import { flatRoutes } from 'remix-flat-routes'
+
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+export default {
+	cacheDirectory: './node_modules/.cache/remix',
+	ignoredRouteFiles: ['**/*'],
+	serverModuleFormat: 'esm',
+	serverPlatform: 'node',
+	serverDependenciesToBundle: ['ra-data-simple-rest'],
+	tailwind: true,
+	postcss: true,
+	watchPaths: ['./tailwind.config.ts'],
+	routes: async defineRoutes => {
+		return flatRoutes('routes', defineRoutes, {
+			ignoredRouteFiles: [
+				'.*',
+				'**/*.css',
+				'**/*.test.{js,jsx,ts,tsx}',
+				'**/__*.*',
+			],
+		})
+	},
+}
